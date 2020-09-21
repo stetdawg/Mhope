7134cea2907aa33c288e1e7bc22a117a46090104
stetdawg <stetdawg101@gmail.com>
2020-09-20 22:12:31 -0700

    react-responsive-gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,5 @@
 import React,{ Component } from 'react'
-import ResponsiveGallery from 'react-responsive-gallery-cloned'
-import { FixedSizeList } from 'react-window'
+import ResponsiveGallery from 'react-responsive-gallery'
 import importAll from 'import-all.macro'
 import '../scss/Gallery.scss'
     
