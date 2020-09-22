9e0d94401615d9960ae0cf95fa16c4b809c93517
stetdawg <stetdawg101@gmail.com>
2020-09-22 09:50:39 -0700

    package.json again

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react'
-import ResponsiveGallery from 'react-responsive-gallery'
+import ResponsiveGallery from 'react-responsive-gallery-cloned'
 import importAll from 'import-all.macro'
 import '../scss/Gallery.scss'
     
