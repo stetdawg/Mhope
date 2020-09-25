0ee7dcb349a06c03e9c52ae8f974a10b40def8ca
stetdawg <stetdawg101@gmail.com>
2020-09-24 23:00:38 -0700

    fixed form.

--- src/components/EmailCapture.js
+++ src/components/EmailCapture.js
@@ -14,7 +14,7 @@ const EmailCapture = () => <div id="email-capture">
 
             <p>Please join our mailing list today!
             We'll keep in touch and keep you informed.</p>
-               <form name="contact" method="POST" data-netlify="true" id="form">
+               <form name="contact" method="POST" id="form">
                <input type="hidden" name="form-name" value="contact" />
                 <label className="label" name="email" htmlFor="email">Email: </label>
                 <input type="text" id=""/>
