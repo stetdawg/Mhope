2462991fb5a89348c5b1b8671b9fb5b57e04b426
stetdawg <stetdawg101@gmail.com>
2020-09-24 22:48:03 -0700

    fixed Chrome based glitches. Functioning form.

--- src/components/EmailCapture.js
+++ src/components/EmailCapture.js
@@ -4,14 +4,23 @@ import '../scss/EmailCapture.scss'
 
 
 const EmailCapture = () => <div id="email-capture">
+    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+    <input type="email" name="email" />
+
+      <input type="text" name="name" />
+      <textarea name="message"></textarea>
+    </form>
+
+
             <p>Please join our mailing list today!
             We'll keep in touch and keep you informed.</p>
-               <div id="form">
-                <label htmlFor="email">Email: </label>
+               <form name="contact" method="POST" data-netlify="true" id="form">
+               <input type="hidden" name="form-name" value="contact" />
+                <label className="label" name="email" htmlFor="email">Email: </label>
                 <input type="text" id=""/>
-                <label htmlFor="name">Name: </label>
+                <label className="label" name="name" htmlFor="name">Name: </label>
                 <input type="text" id=""/>
-               </div> 
+               </form> 
             </div>
 
 export default EmailCapture
