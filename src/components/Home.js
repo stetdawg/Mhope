831934314acc908df7dbaf40de717bc141dcacdf
stetdawg <stetdawg101@gmail.com>
2020-10-30 14:07:17 -0700

    form

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,6 +29,11 @@ render(){
 return (
 <div id="home">
      <Hero />
+     <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
+</form>
     <EmailCapture />
         <div id="gallery">
             {
